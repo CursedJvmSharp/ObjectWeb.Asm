2d2cd1f39bd2ae5d72572cf81b1ce1f6c07ea4b1
NickAcPT <32451103+NickAcPT@users.noreply.github.com>
2021-08-28 14:43:39 +0100

    Cleanup step #9 - Sort modifiers

--- CursedJvmSharp.Workspace/Program.cs
+++ CursedJvmSharp.Workspace/Program.cs
@@ -1,20 +1,16 @@
-﻿using System;
-using System.Diagnostics;
-using System.IO;
-using System.Runtime.CompilerServices;
+﻿using System.IO;
 using ObjectWeb.Asm;
 using ObjectWeb.Asm.Tree;
 
 var node = new ClassNode();
 
-new ClassReader(Unsafe.As<byte[]>(File.ReadAllBytes(@"D:\Downloads\Swapchain.class"))).Accept(node, 0);
-
-Console.WriteLine(node);
-//Debugger.Break();
+new ClassReader(File.ReadAllBytes(@"D:\Downloads\Swapchain.class")).Accept(node, 0);
 
 var classVisitor = new ClassWriter(0);
 node.Accept(classVisitor);
 
 var byteArray = classVisitor.ToByteArray();
 
-File.WriteAllBytes(@"D:\Downloads\Swapchain-out.class", Unsafe.As<byte[]>(byteArray));
+File.WriteAllBytes(@"D:\Downloads\Swapchain-out.class", byteArray);
+
+
